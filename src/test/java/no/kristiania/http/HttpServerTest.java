6b8f5a8a8656895dd805793815b137a6bb5550f4
Ørjan Skotnes <69791988+OrjanSkotnes@users.noreply.github.com>
2021-10-29 11:04:59 +0200

    :test_tube: Added failing test shouldReadFileFromDisk

--- src/test/java/no/kristiania/http/HttpServerTest.java
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -3,6 +3,9 @@ package no.kristiania.http;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,6 +26,15 @@ class HttpServerTest {
     void shouldIncludeRequestTargetIn404Message() throws IOException {
         HttpClient client = new HttpClient("localhost", server.getPort(), "/does-not-exist");
         assertEquals("File not found /does-not-exist", client.getMessageBody());
+    }
+
+    @Test
+    void shouldReadFileFromDisk() throws IOException {
+        String fileContent = "A file created at " + LocalDateTime.now();
+        Files.write(Paths.get("target/test-classes/example-file.txt"), fileContent.getBytes());
+        server.setRoot(Paths.get("target/test-classes"));
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/example-file.txt");
+        assertEquals(fileContent, client.getMessageBody());
     }
 
 
