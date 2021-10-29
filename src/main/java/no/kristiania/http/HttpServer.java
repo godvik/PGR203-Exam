037da372aa9462d680299db575e98db6dd5e4dfc
godvik <32129757+godvik@users.noreply.github.com>
2021-10-29 10:41:05 +0200

    :white_check_mark: Passing test shouldReturn404ForUnknownRequestTarget

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -1,6 +1,32 @@
 package no.kristiania.http;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
 
 
+    private ServerSocket serverSocket;
+
+    public HttpServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+
+        new Thread(this::handleClients).start();
+    }
+
+    private void handleClients() {
+        try {
+            Socket clientSocket = serverSocket.accept();
+            String responseText = "File not found";
+            String response = "HTTP/1.1 404 " + responseText + "\r\n" +
+                    "Content-Length: " + responseText.getBytes().length + "\r\n" +
+                    "Connection: close" + "\r\n" +
+                    "\r\n" +
+                    responseText;
+            clientSocket.getOutputStream().write(response.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
