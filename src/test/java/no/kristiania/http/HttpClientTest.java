3e22514c231b37bf9f143f8c9233a045d65a129d
Ørjan Skotnes <69791988+OrjanSkotnes@users.noreply.github.com>
2021-10-29 09:33:11 +0200

    :test_tube: Added failing test shouldRecieveStatus200

--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -1,7 +1,14 @@
 package no.kristiania.http;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpClientTest {
 
+    @Test
+    void shouldRecieveStatus200() {
+        HttpClient client = new HttpClient("httpbin.org", 80, "status/200");
+        assertEquals(200, client.getStatusCode());
+    }
 }
