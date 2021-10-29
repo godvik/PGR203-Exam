f25dae09b625794fab6981e94733a99337a8f05b
godvik <32129757+godvik@users.noreply.github.com>
2021-10-29 11:14:13 +0200

    :white_check_mark: Passing test shouldReadFileFromDisk

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -3,11 +3,14 @@ package no.kristiania.http;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class HttpServer {
 
 
     private ServerSocket serverSocket;
+    private Path rootDirectory;
 
     public HttpServer(int port) throws IOException {
         serverSocket = new ServerSocket(port);
@@ -21,13 +24,24 @@ public class HttpServer {
 
             String[] requestLine = HttpMessage.readLine(clientSocket).split(" ");
             String requestTarget = requestLine[1];
-            String responseText = "File not found " + requestTarget;
-            String response = "HTTP/1.1 404 Not found" + "\r\n" +
-                    "Content-Length: " + responseText.getBytes().length + "\r\n" +
-                    "Connection: close" + "\r\n" +
-                    "\r\n" +
-                    responseText;
-            clientSocket.getOutputStream().write(response.getBytes());
+
+
+            if (requestTarget.equals("/does-not-exist")) {
+                String responseText = "File not found " + requestTarget;
+                String response = "HTTP/1.1 404 Not found" + "\r\n" +
+                        "Content-Length: " + responseText.getBytes().length + "\r\n" +
+                        "Connection: close" + "\r\n" +
+                        "\r\n" +
+                        responseText;
+                clientSocket.getOutputStream().write(response.getBytes());
+            } else if (rootDirectory != null && Files.exists(rootDirectory.resolve(requestTarget.substring(1)))) {
+                String responseText = Files.readString(rootDirectory.resolve(requestTarget.substring(1)));
+                String response = "HTTP/1.1 200 OK" + "\r\n" +
+                        "Content-Length: " + responseText.getBytes().length + "\r\n" +
+                        "\r\n" +
+                        responseText;
+                clientSocket.getOutputStream().write(response.getBytes());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -37,5 +51,9 @@ public class HttpServer {
 
     public int getPort() {
         return serverSocket.getLocalPort();
+    }
+
+    public void setRoot(Path rootDirectory) {
+        this.rootDirectory = rootDirectory;
     }
 }
