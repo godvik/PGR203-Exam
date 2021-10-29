048330dffd08ba8976d8cce71b05684ff039f925
godvik <32129757+godvik@users.noreply.github.com>
2021-10-29 09:44:42 +0200

    :white_check_mark: Passing test shouldRecieveStatus200

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -1,5 +1,34 @@
 package no.kristiania.http;
 
-public class HttpClient {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 
+public class HttpClient {
+    private final int statusCode;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "\r\n";
+        socket.getOutputStream().write(request.getBytes());
+
+        int c;
+        StringBuilder result = new StringBuilder();
+        InputStream inputStream = socket.getInputStream();
+        while ((c = inputStream.read()) != -1 && c != '\r') {
+            result.append((char)c);
+        }
+        int expectedNewLine = socket.getInputStream().read();
+        assert expectedNewLine == '\n';
+
+        this.statusCode = Integer.parseInt(result.toString().split(" ")[1]);
+
+
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
 }
--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -2,13 +2,15 @@ package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpClientTest {
 
     @Test
-    void shouldRecieveStatus200() {
-        HttpClient client = new HttpClient("httpbin.org", 80, "status/200");
+    void shouldRecieveStatus200() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/status/200");
         assertEquals(200, client.getStatusCode());
     }
 }
